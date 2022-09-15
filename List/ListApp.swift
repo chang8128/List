24135fb9011a3ba3a2423f1097a4e35a1b37233b
czp356 <czp356@gmail.com>
2022-09-15 22:05:53 +0800

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by 常志平 on 15/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
